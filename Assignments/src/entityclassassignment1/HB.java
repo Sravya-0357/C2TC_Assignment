81c04d7d7825fe6926ff605ef0f9c2dcfef38b2c
DELL <DELL@SravyaPC2>
2025-07-22 13:51:00 +0530

    My Second Assignment Question

---
+++ Assignments/src/entityclassassignment1/HB.java
@@ -0,0 +1,30 @@
+package entityclassassignment1;
+
+public class HB {
+	private int count;
+	private String name;
+	private String Product;
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getProduct() {
+		return Product;
+	}
+	public void setProduct(String product) {
+		Product = product;
+	}
+	@Override
+	public String toString() {
+		return "HB count=" + count + ", name=" + name + ", Product=" + Product;
+	}
+	
+}
---
+++ Assignments/src/entityclassassignment1/HBDetails.java
@@ -0,0 +1,32 @@
+package entityclassassignment1;
+
+public class HBDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HB h1 = new HB();
+		h1.setCount(7);
+		h1.setName("Choclate");
+		h1.setProduct("Dairy milk");
+		System.out.println(h1);
+		
+		HB h2 = new HB();
+		h2.setCount(5);
+		h2.setName("Cake");
+		h2.setProduct("Brownie");
+		System.out.println(h2);
+		
+		HB h3 = new HB();
+		h3.setCount(3);
+		h3.setName("Biryani");
+		h3.setProduct("Hyderabad Biryani");
+		System.out.println(h3);
+		
+		HB h4 = new HB();
+		h4.setCount(1);
+		h4.setName("Starters");
+		h4.setProduct("Pav Bhajji");
+		System.out.println(h4);
+	}
+
+}
